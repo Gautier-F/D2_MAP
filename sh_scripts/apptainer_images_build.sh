67c9518a52266b06c67292715afcf6d5c7bb76a7
Fabien Gautier <fabien.gautier@univ-nantes.fr>
2026-02-19 11:36:17 +0100

    add sh script to build apptainer images

---
+++ sh_scripts/apptainer_images_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script builds the Apptainer images for D2_MAP. It should be run from the root of the D2_MAP repository.
+
+path_to_d2_map_root=$(pwd)
+
+export APPTAINER_CACHEDIR=$path_to_d2_map_root/.apptainer_cache
+export TMPDIR=$path_to_d2_map_root/tmp
+mkdir -p $APPTAINER_CACHEDIR $TMPDIR 
+mkdir -p $APPTAINER_CACHEDIR/containers
+
+# Build the Apptainer images
+module load apptainer
+
+apptainer pull $path_to_d2_map_root/containers/dorado.sif docker://nanoporetech/dorado@sha256:6156fdbb48ff13fbb141b4f1fc6e6300f05221ecfdd114fc8323a0e38296c1dd
+apptainer pull $path_to_d2_map_root/containers/multiqc.sif docker://multiqc/multiqc@sha256:a23ffc66f702b7d426634a8daa5314107f996fa0a93ae976b755531a992ebd61
+
+apptainer build --fakeroot $path_to_d2_map_root/containers/modkit_v6_bedtools.sif containers/modkit_bedtools.def
+apptainer build --fakeroot $path_to_d2_map_root/container/R_extended.sif containers/r_base_extended.sif
+
+rm -rf $APPTAINER_CACHEDIR
+rm -rf $TMPDIR
